23feb8465863aa473048ca40ede651356d977ac3
COATZ <co.artizzu@gmail.com>
2022-03-16 09:39:25 +0100

    Update 22/03/15

---
+++ baseline/exp3_2d3ds/run_data2_sphe_fcnet.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+MESHFILES=../../mesh_files
+DATADIR=data2_sphe
+
+if [ ! -d $MESHFILES ]; then
+    echo "[!] Mesh files do not exist..."
+    exit
+fi
+
+# generate data
+if [ ! -d $DATADIR ]; then
+    echo "[!] Data files do not exist. Preparing data..."
+    # download preprocessed spherical data
+	wget --no-check-certificate http://island.me.berkeley.edu/ugscnn/data/2d3ds_pano_small.zip
+
+	# setup data
+	unzip 2d3ds_pano_small.zip
+	mv 2d3ds_pano_small data
+	rm 2d3ds_pano_small.zip
+fi
+
+# create log directory
+mkdir -p logs
+
+#source activate
+
+python3 train.py \
+--batch-size 16 \
+--test-batch-size 16 \
+--epochs 200 \
+--data_folder data2_sphe \
+--fold 3 \
+--log_dir logs/log_FCN8s_f16_cv3_rgbd_data2_sphe \
+--decay \
+--train_stats_freq 5 \
+--model FCN8s \
+--in_ch rgbd \
+--lr 1e-3 \
+--feat 16
+
+# FCN8s, UNet
+
